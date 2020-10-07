5a2706f6ed29761bc7ef6a730eba83c6dfaa7524
Legend <martjo.expert@gmail.com>
2020-10-07 16:48:37 +0100

    ClassBasedViews_Usedforcreate_halls

--- halls/views.py
+++ halls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .models import Hall
 
 # Create your views here.
 def home(request):
@@ -15,3 +16,16 @@ class SignUp(generic.CreateView ):
         else if you use something differnt you will have to created a custome view for that to work
     '''
     template_name = 'registration/signup.html'
+
+"""
+1-get the Halls models
+2-set the fields
+3-template for the creat
+4-If create is sucess, redirect to home page
+
+"""
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
