fe0416ab823d33d7d3c362a8be16d7e36c006c5c
Legend <martjo.expert@gmail.com>
2020-10-07 19:03:05 +0100

    ClassbasedViewCustomizationWith_form_valid()-Method

--- halls/views.py
+++ halls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
@@ -7,7 +7,6 @@ from .models import Hall
 # Create your views here.
 def home(request):
     return render(request, 'halls/home.html')
-
 
 class SignUp(generic.CreateView ):
     form_class = UserCreationForm
@@ -22,10 +21,23 @@ class SignUp(generic.CreateView ):
 2-set the fields
 3-template for the creat
 4-If create is sucess, redirect to home page
-
 """
 class CreateHall(generic.CreateView):
     model = Hall
     fields = ['title']
     template_name = 'halls/create_hall.html'
     success_url = reverse_lazy('home')
+
+    # user associated with creating a hall
+    """
+    1-Use Djnago inbuilt form_valid as function name
+    2-Get the user object
+    3-Use super i.e the generic class of form_valid to validate
+    4-Redirect home
+    https://docs.djangoproject.com/en/2.2/topics/class-based-views/generic-editing/#basic-forms
+    https://docs.djangoproject.com/en/2.2/ref/class-based-views/generic-editing/#createview
+    """
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home') # we will later correct and redirect to a user' halls
