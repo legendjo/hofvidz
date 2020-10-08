4365a6020b6e395c4032ca9cdf825b6a12073e8e
Legend <martjo.expert@gmail.com>
2020-10-08 12:28:41 +0100

    AutoLoginaUserAfterSignUp

--- halls/views.py
+++ halls/views.py
@@ -2,19 +2,36 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
 
 # Create your views here.
 def home(request):
     return render(request, 'halls/home.html')
 
+
+'''It is important to use [registration] here its Djnago's registration keyword,
+    else if you use something differnt you will have to created a custome view for that to work
+
+    #Login a SignUp User After SignUp
+    1-Use form_valid
+    2-get the view
+    3-get username and password
+    4-authenticate user
+    5-login the user
+    return view
+'''
 class SignUp(generic.CreateView ):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
-    '''It is important to use [registration] here its Djnago's registration keyword,
-        else if you use something differnt you will have to created a custome view for that to work
-    '''
     template_name = 'registration/signup.html'
+    #Login a SignUp User After SignUp
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request,user)
+        return view
 
 """
 1-get the Halls models
